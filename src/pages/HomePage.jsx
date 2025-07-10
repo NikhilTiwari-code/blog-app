754dc6e16321da44bec14b714ddc1edea418e331
Your Name <nikhiltiwari0296@gmail.com>
2025-07-10 16:08:50 +0530

    fixed user post

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,9 +11,6 @@ function HomePage() {
     
     useEffect(() => {
         const baseQuery = [Query.equal("status", "active")];
-        if (isLoggedIn && userData?.$id) {
-            baseQuery.push(Query.equal("userId", userData.$id));
-        }
         appwriteService.getPosts(baseQuery).then((posts)=>{
           if(posts){
             setPosts(posts.documents)
